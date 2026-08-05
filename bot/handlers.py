17463662ec0f3db5b3608d0ac41d2077f961f6be
Vlad <vlad.kolomietc8@gmail.com>
2026-08-05 23:31:07 +0300

    feat: add bot directory, core logic and initial handlers

---
+++ bot/handlers.py
@@ -0,0 +1,17 @@
+from aiogram import Router, F 
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Hey!')
+
+@router.message(Command('help'))
+async def get_help(message: Message):
+    await message.answer('An automated Telegram bot that tracks focus time by parsing iOS lock screen screenshots. It extracts timer data and currently playing music, providing visual analytics and productivity statistics')
+
+@router.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f'ID photo: {message.photo[-1].file_id}')
---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+
+from bot.config import TOKEN
+from bot.handlers import router
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    
+    dp.include_router(router)
+    
+    print("Бот успішно запущено!")
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
+    
